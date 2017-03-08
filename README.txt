774c5d91922036875f4552d36bab2b346fb9f3e2
betonildo <gilbertoribeiropazdarosa@gmail.com>
2017-03-08 07:25:58 -0300

    [add] Windows compilation to include windows entry point

--- README.txt
+++ README.txt
@@ -1,1 +1,4 @@
 For g++ on windows use -municode for compilation.
+
+Windows compilation to include windows entry point
+ g++ src\appmain.cpp src\main.cpp src\WindowManager.cpp src\Input.cpp -o main -I"include/" -municod
--- src/appmain.cpp
+++ src/appmain.cpp
@@ -1,8 +1,9 @@
-#include "main.h"
+//#include "main.h"
+#include <windows.h>
+extern int main();
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow) {
     // Run the message loop.
     // TODO: Parse cmd line as argv and count it as argc to pass to main definition
-    printf("%d\n", nCmdShow);
     return main();
 }
