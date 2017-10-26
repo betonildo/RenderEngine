2e0d85fbe15c5b748b887a56b6822a68202ea676
Gilberto <gilbertoribeiropazdarosa@gmail.com>
2017-10-26 18:01:04 -0200

    [chg] class diagram

--- test/main.cpp
+++ test/main.cpp
@@ -1,4 +1,36 @@
+#include <iostream>
+#include <string.h>
+class MyType {
+
+public:
+    float d1, d2, d3;
+};
+
+std::ostream& operator<<(std::ostream& os, const MyType& mt) {
+    os << mt.d1 << " " << mt.d2 << " " << mt.d3;
+    return os;
+}
+
+template <class GLApiImpl>
+class GLApi {
+
+};
+
+
+class OpenGL {
+
+};
+
+typedef GLApi<OpenGL> GL;
+
 int main( int argc, char **argv ) {
-   
+
+    MyType mt;
+    float asd[] = {2.0, 3.0, 4.0};
+    memcpy(&mt, asd, sizeof(MyType));
+
+    std::cout << mt << std::endl;
+
+
     return 0;
 }
